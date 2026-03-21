963da9268bd7aee8c90716f30b459fa41f24e94a
javacommons <javacommons@gmail.com>
2026-03-21 09:35:54 +0900

    ⭕️ Commit at 2026.0321.0935.53 [https://github.com/nuget-pkg/UniversalEncoding] [DIFF] diff --git a/UniversalEncoding/UniversalEncoding.cs b/UniversalEncoding/UniversalEncoding.cs     +using System.Text.RegularExpressions;     -public class UniversalEncoding     -{     -    public static int Add2(int a, int b)     -    {     +public class UniversalEncoding {     +    public static int Add2(int a, int b) {     +    public static string ReplaceSurrogatePair(string str, string replaceSurrogate = "★") {     +        if (replaceSurrogate == "") {     +            return str;     +        }     +        str = Regex.Replace(str, @"[\uD800-\uDFFF]", "{ddbea68e-d93f-4e85-92b5-83b1ace6d50f}");     +        str = str.Replace("{ddbea68e-d93f-4e85-92b5-83b1ace6d50f}{ddbea68e-d93f-4e85-92b5-83b1ace6d50f}", replaceSurrogate);     +        str = str.Replace("{ddbea68e-d93f-4e85-92b5-83b1ace6d50f}", replaceSurrogate);     +        return str;     +    }     +    public static string AdjustFileName(string fileName, string replaceSurrogate = "★") {     +        fileName = fileName     +            .Replace("!", "❢")     +            .Replace("！", "❢")     +            .Replace("\"", "“")     +            .Replace("'", "‘")     +            .Replace("#", "＃")     +            .Replace("%", "％")     +            .Replace("&", "＆")     +            .Replace("(", "｟")     +            .Replace(")", "｠")     +            .Replace("（", "｟")     +            .Replace("）", "｠")     +            .Replace("^", "＾")     +            .Replace("~", "～")     +            .Replace("\\", "＼")     +            .Replace("|", "￤")     +            .Replace("｜", "￤")     +            .Replace("`", "｀")     +            .Replace(";", "；")     +            .Replace(":", "：")     +            .Replace("*", "＊")     +            .Replace("[", "⁅")     +            .Replace("]", "⁆")     +            .Replace("［", "⁅")     +            .Replace("］", "⁆")     +            .Replace("{", "〘")     +            .Replace("}", "〙")     +            .Replace("｛", "〘")     +            .Replace("｝", "〙")     +            .Replace("<", "≪")     +            .Replace(">", "≫")     +            .Replace("＜", "≪")     +            .Replace("＞", "≫")     +            .Replace("/", "／")     +            .Replace("?", "❔")     +            .Replace("？", "❔")     +            .Replace("　", " ")     +            ;     +        fileName = ReplaceSurrogatePair(fileName, replaceSurrogate);     +        return fileName;     +    }     +    public static string AdjustMetaData(string metadata, string replaceSurrogate = "★") {     +        metadata = metadata     +            .Replace("\"", "“")     +            .Replace("'", "‘")     +            .Replace("\\", "＼")     +            ;     +        metadata = ReplaceSurrogatePair(metadata, replaceSurrogate);     +        return metadata;     +    } [/DIFF]

--- UniversalEncoding/UniversalEncoding.cs
+++ UniversalEncoding/UniversalEncoding.cs
@@ -5,15 +5,74 @@ using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 // ReSharper disable once CheckNamespace
 namespace Global;
 
 // ReSharper disable once InconsistentNaming
-public class UniversalEncoding
-{
-    public static int Add2(int a, int b)
-    {
+public class UniversalEncoding {
+    public static int Add2(int a, int b) {
         return a + b;
+    }
+    public static string ReplaceSurrogatePair(string str, string replaceSurrogate = "★") {
+        if (replaceSurrogate == "") {
+            return str;
+        }
+        str = Regex.Replace(str, @"[\uD800-\uDFFF]", "{ddbea68e-d93f-4e85-92b5-83b1ace6d50f}");
+        str = str.Replace("{ddbea68e-d93f-4e85-92b5-83b1ace6d50f}{ddbea68e-d93f-4e85-92b5-83b1ace6d50f}", replaceSurrogate);
+        str = str.Replace("{ddbea68e-d93f-4e85-92b5-83b1ace6d50f}", replaceSurrogate);
+        return str;
+    }
+    public static string AdjustFileName(string fileName, string replaceSurrogate = "★") {
+        fileName = fileName
+            .Replace("!", "❢")
+            .Replace("！", "❢")
+            .Replace("\"", "“")
+            .Replace("'", "‘")
+            .Replace("#", "＃")
+            .Replace("%", "％")
+            .Replace("&", "＆")
+            .Replace("(", "｟")
+            .Replace(")", "｠")
+            .Replace("（", "｟")
+            .Replace("）", "｠")
+            .Replace("^", "＾")
+            .Replace("~", "～")
+            .Replace("\\", "＼")
+            .Replace("|", "￤")
+            .Replace("｜", "￤")
+            .Replace("`", "｀")
+            .Replace(";", "；")
+            .Replace(":", "：")
+            .Replace("*", "＊")
+            .Replace("[", "⁅")
+            .Replace("]", "⁆")
+            .Replace("［", "⁅")
+            .Replace("］", "⁆")
+            .Replace("{", "〘")
+            .Replace("}", "〙")
+            .Replace("｛", "〘")
+            .Replace("｝", "〙")
+            .Replace("<", "≪")
+            .Replace(">", "≫")
+            .Replace("＜", "≪")
+            .Replace("＞", "≫")
+            .Replace("/", "／")
+            .Replace("?", "❔")
+            .Replace("？", "❔")
+            .Replace("　", " ")
+            ;
+        fileName = ReplaceSurrogatePair(fileName, replaceSurrogate);
+        return fileName;
+    }
+    public static string AdjustMetaData(string metadata, string replaceSurrogate = "★") {
+        metadata = metadata
+            .Replace("\"", "“")
+            .Replace("'", "‘")
+            .Replace("\\", "＼")
+            ;
+        metadata = ReplaceSurrogatePair(metadata, replaceSurrogate);
+        return metadata;
     }
 }
